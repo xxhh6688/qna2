6ea2e3a97f92c3b7f68dbba204951b9a0ed880a5
Liang Xu (Wicresoft) <v-lixu@microsoft.com>
2021-01-28 23:27:55 +0800

    change static path

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(cookieParser());
-app.use(express.static('js'));
-app.use(express.static('image'));
+app.use(express.static('/'));
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
@@ -21,7 +20,7 @@ require("./router.js")(app);
 
 // simple route
 app.get("/", (req, res) => {
-    res.writeHead(200, {'Content-Type':'text/html'})
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     fs.readFile('index.html',function(error, data){
         if(error){
             res.writeHead(404);
